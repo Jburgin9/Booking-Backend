1709f643de1a67d7ff43f1fb919e862b91f5ecad
Jburgin9 <Justin.Burgin@gmail.com>
2023-08-14 16:21:18 -0400

    2 new mongo tests included

--- mongo.test.js
+++ mongo.test.js
@@ -1,34 +1,51 @@
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const { MongoClient } = require('mongodb');
+const { Jester } = require('./jester');
 
-describe('insert', () => {
+describe('mongo operations', () => {
     let connection;
     let db;
-
+    const jester = new Jester()
+    let mongod 
 
     beforeAll(async () => {
-
-        // This will create an new instance of "MongoMemoryServer" and automatically start it
-        const mongod = await MongoMemoryServer.create()
-
-        const uri = mongod.getUri();
-        connection = await new MongoClient(uri, { useNewUrlParser: true })
+        // console.log(jester.getMongo().then((res) => {res}))
+        // const uri = mongod.getUri();
+        connection = await new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true })
         db = await connection.db("JESTER");
-        return mongod
     });
 
     it('should insert a doc into collection', async () => {
         console.log(`test: ${connection}`)
-        const users = db.collection('testing');
+        const testingCollection = db.collection('testing');
 
-        const mockUser = { _id: '2', name: 'John' };
-        await users.insertOne(mockUser);
+        const mockUser = { _id: '1', name: 'John' };
+        await testingCollection.insertOne(mockUser);
 
-        const insertedUser = await users.findOne({ _id: '2' });
+        const insertedUser = await testingCollection.findOne({ _id: '1' });
         expect(insertedUser).toEqual(mockUser);
     });
 
+    it('should retrieve specific users', async() => {
+        //search for user by id
+        const testingCollection = db.collection('testing');
+        const mockUser = { _id: '1', name: 'John' };
+        const searchUser = await testingCollection.findOne({ _id: '1' });
+        expect(searchUser).toEqual(mockUser)
+
+    })
+
+    it('should retrieve all users', async() => {
+        //check for user count and retrieve users. if user list match the count of records in the collection the test should pass
+        const testingCollection = db.collection('testing');
+        const collectionCount = await testingCollection.count()
+        console.log(`ALL: ${collectionCount}`)
+        expect(collectionCount).toEqual(1)
+    })
+
     afterAll(async () => {
+        // await mongod.stop()
         await connection.close();
     });
 });
+
