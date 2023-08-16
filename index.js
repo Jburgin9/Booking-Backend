f940147dc6049532822fc1ce8654eeab343fc9df
Jburgin9 <Justin.Burgin@gmail.com>
2023-08-15 20:51:27 -0400

    Added GraphQL query

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require("cors")
-const SairDB = require('./SairDB')
-
 
 app.listen(process.env.PORT, () => {
     console.log(`running on ${process.env.MONGO_URI}`); 
